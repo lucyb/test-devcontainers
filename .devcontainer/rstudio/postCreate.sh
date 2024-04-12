d9484257ea391d1f50800c583d89213b96df3ba0
Jon Massey <jon.massey@thedatalab.org>
2024-04-12 13:08:46 +0000

    Add ehrql and python conf

--- .devcontainer/rstudio/postCreate.sh
+++ .devcontainer/rstudio/postCreate.sh
@@ -6,3 +6,8 @@ pip3 install -r .devcontainer/requirements.in
 
 #set RStudio working directory
 ! grep -q `pwd` ~/.config/rstudio/rstudio-prefs.json && cat ~/.config/rstudio/rstudio-prefs.json | jq ". + {\"initial_working_directory\":\"`pwd`\"}" >  ~/.config/rstudio/rstudio-prefs.json 
+
+#download and extract latest ehrql source
+wget https://github.com/opensafely-core/ehrql/archive/main.zip -P .devcontainer
+unzip .devcontainer/main.zip -d .devcontainer/
+rm .devcontainer/main.zip
