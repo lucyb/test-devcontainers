215105dd6c82a60d586be147de38738b102fd33e
Jon Massey <jon.massey@thedatalab.org>
2024-04-12 12:47:48 +0000

    Set R and RStudio working directory

---
+++ .devcontainer/rstudio/postCreate.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+pip3 install -r .devcontainer/requirements.in
+
+#set R working directory
+! grep -q `pwd` $R_HOME/etc/Rprofile.site && sudo tee -a $R_HOME/etc/Rprofile.site <<< "setwd(\"`pwd`\")"
+
+#set RStudio working directory
+! grep -q `pwd` ~/.config/rstudio/rstudio-prefs.json && cat ~/.config/rstudio/rstudio-prefs.json | jq ". + {\"initial_working_directory\":\"`pwd`\"}" >  ~/.config/rstudio/rstudio-prefs.json 
